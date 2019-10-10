2b76cd2cc45783c63c72cf9f77d3264ad46c4862
uliseshrrs <ulipsv@comunidad.unam.mx>
2019-10-09 23:23:17 -0500

    archivos2

---
+++ Libro/EJEMPLO1.10.py
@@ -0,0 +1,8 @@
+#ULISES HERNANDEZ SANCHEZ
+
+BASE = float ( input (" Escribe la base del rectangulo: " ))
+ALTURA = float ( input (" Escribe la altura del rectangulo: " ))
+SUPERFICIE = BASE * ALTURA
+PERIMETRO = 2*(BASE + ALTURA)
+print(f"Un cuadrado de base {BASE} y altura {ALTURA} su superficie es {SUPERFICIE}")
+print(f"Un cuadrado de base {BASE} y altura {ALTURA} su perimetro es {PERIMETRO}")
