b5134409b3b233cd14e272a4962540f89d14eaba
uliseshrrs <ulipsv@comunidad.unam.mx>
2019-10-22 17:45:00 -0500

    clase del 22 de octubre

---
+++ Listas.py
@@ -0,0 +1,23 @@
+#listas
+lluvias_norte=[80,60,120,100,70,150,100,47,95,70,100,130]# Cada espacio representa el mes
+
+for indice in range(0,12,1):  #Representa el rango 
+    print(f" mes { indice+1 } en region norte={ lluvias_norte[indice]}")
+    
+print(lluvias_norte[4])# Imprime en pantalla de la variable lluvia_norte
+print(lluvias_norte[:5:])
+
+sueldos = []
+suma = 0
+for indice in range (7):
+    sueldos.append(int(input("sueldo:")))
+print(sueldos)
+
+for indice in range(7):
+     suma+=sueldos[indice]
+pomedio = suma / 7
+cont = 0
+for indice in range (7):
+    if sueldos [indice]> promedio:
+        cont +=1
+        print(f"Arriva:{sueldos[indice] }")
