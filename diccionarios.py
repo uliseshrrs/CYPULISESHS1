7d02ffb0cfd8b90d73a52713a5131a7a3b3bfa0c
uliseshrrs <ulipsv@comunidad.unam.mx>
2019-11-05 17:51:57 -0600

    clase del 5 de noviembre

---
+++ diccionarios.py
@@ -0,0 +1,31 @@
+#diccionarios {'llave':valor'}
+alumno={'num_cta':'20164757473',
+       'nombre':('jose','perez','Garcia'),
+       'semestre':1,
+       'promedio':0.0,
+       'materias':['CYP', 'algebra','calculo','calculo','geometria','introICO'],
+       'regular': True,
+       'lagrimas_por_examen': 5,
+        'direccion':{
+        'calle',:'Rancho sequito',
+        'colonia':'impulsora',
+        'numero':1000,
+        'cp':7570,
+        'del_mun':'Nezahualcoyotl',
+        'estado':{
+                'id': 15,
+                'nombre':'estado de mexico',
+                'corto':'edomex'
+                }
+        }
+      }
+print(alumno['materias'][1].upper)
+print(alumno['nombre'])
+print(alumno)
+print(alumno['nombre'][1])
+print(alumno['direccion'][]'cp'])
+print(alumno['direccion'][]'estado']['croto'])
+print(alumno['materias'][3::])
+
+mi_lista=[
+
---
+++ tuplas.py
@@ -0,0 +1,7 @@
+#tuplas
+#se declara con "("y")"
+#son imitables y organizan datos igual que las listas
+datos_db = ('132.248.173.88','root','123456789')
+print(datos_db[1])
+datos_db = ('132.248.173.88','admin','123456789')
+print(datos_db[1])        
