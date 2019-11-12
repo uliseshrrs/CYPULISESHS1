a29bd3a62a1d2e23bb80efcdec96bcb83750b144
uliseshrrs <ulipsv@comunidad.unam.mx>
2019-11-12 17:57:17 -0600

    clase del 12 de nov

---
+++ leer_numeros.py
@@ -0,0 +1,31 @@
+archivo= open ("numeros.txt","rt" )
+print(dir(archivo))
+
+numeros1 = archivo.read()
+print(numeros1)
+print(numeros1.split('\n'))
+listas_num[]
+for linea in numero1.aplit('\n'):
+    for numero in linea.split(','):
+        lista_num.append(int (numero) )
+print(lista_num)
+lista_num.sort()
+print(f"\n lista ordenada : {listas_num}\n")
+print(f"\n lista ordenada : {listas_num}\n")
+
+
+
+    
+
+archivo.close()
+
+archivo= open ("numeros.txt","rt" )
+numero2 = archivo.readlines()
+print(numeros2)
+archivo.close()
+
+archivo= open ("numeros.txt","rt" )
+numero2 = archivo.readlines()
+print(numeros2)
+archivo.close()
+
